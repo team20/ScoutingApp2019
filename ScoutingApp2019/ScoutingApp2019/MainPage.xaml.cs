ebbbbc4bd548d503761a80071ce38e4de3798302
Andrew S <yipten@outlook.com>
2019-03-01 20:54:43 -0500

    finished changing method used for using the counters to be able to be written to a file

--- ScoutingApp2019/ScoutingApp2019/MainPage.xaml.cs
+++ ScoutingApp2019/ScoutingApp2019/MainPage.xaml.cs
@@ -18,276 +18,204 @@ namespace ScoutingApp2019 {
         }
 
         private void SandCargoShipPlus_Clicked(object sender, EventArgs e) {
-            if (data.SandCargoShip < 8) {
-                data.SandCargoShip++;
-                SandCargoShipTotal.Text = data.SandCargoShip.ToString();
-            }
+            if (data.SandCargoShip < 8)
+                SandCargoShipTotal.Text = (++data.SandCargoShip).ToString();
         }
 
         private void SandCargoShipMinus_Clicked(object sender, EventArgs e) {
-            if (data.SandCargoShip > 0) {
-                data.SandCargoShip--;
-                SandCargoShipTotal.Text = data.SandCargoShip.ToString();
-            }
+            if (data.SandCargoShip > 0)
+                SandCargoShipTotal.Text = (--data.SandCargoShip).ToString();
         }
 
         private void SandShipHatchMinus_Clicked(object sender, EventArgs e) {
-            if (data.SandPanelShip > 0) {
-                data.SandPanelShip--;
-                SandCargoShipTotal.Text = data.SandPanelShip.ToString();
-            }
+            if (data.SandPanelShip > 0)
+                SandCargoShipTotal.Text = (--data.SandPanelShip).ToString();
         }
 
         private void SandShipHatchPlus_Clicked(object sender, EventArgs e) {
-            if (data.SandPanelShip < 8) {
-                data.SandPanelShip++;
-                SandCargoShipTotal.Text = data.SandPanelShip.ToString();
-            }
+            if (data.SandPanelShip < 8)
+                SandCargoShipTotal.Text = (++data.SandPanelShip).ToString();
         }
 
-        private void SandRock1TCargoMinus_Clicked(object sender, EventArgs e) {
-            if (data.SandCargoRocket3 > 0) {
-                data.SandCargoRocket3--;
-                sandRock1TCargoTotal.Text = data.SandCargoRocket3.ToString();
-            }
+        private void SandRocketTCargoMinus_Clicked(object sender, EventArgs e) {
+            if (data.SandCargoRocket3 > 0)
+                sandRocketTCargoTotal.Text = (--data.SandCargoRocket3).ToString();
         }
 
-        private void SandRock1TCargoPlus_Clicked(object sender, EventArgs e) {
-            if (data.SandCargoRocket3 < 8) {
-                data.SandCargoRocket3++;
-                sandRock1TCargoTotal.Text = data.SandCargoRocket3.ToString();
-            }
-        }
-        
-        private void SandRock1THatchMinus_Clicked(object sender, EventArgs e) {
-            if (data.SandPanelRocket3 > 0) {
-                data.SandPanelRocket3--;
-                sandRock1THatchTotal.Text = data.SandPanelRocket3.ToString();
-            }
+        private void SandRocketTCargoPlus_Clicked(object sender, EventArgs e) {
+            if (data.SandCargoRocket3 < 4)
+                sandRocketTCargoTotal.Text = (++data.SandCargoRocket3).ToString();
         }
 
-        private void SandRock1THatchPlus_Clicked(object sender, EventArgs e) {
-            if (data.SandPanelRocket3 < 8) {
-                data.SandPanelRocket3++;
-                sandRock1THatchTotal.Text = data.SandPanelRocket3.ToString();
-            }
-        }
-        //TODO: finish this!!!!!!!!!
-        private void SandRock1MCargoMinus_Clicked(object sender, EventArgs e) {
-            if (data.SandCargoRocket2 > 0) {
-
-            }
+        private void SandRocketTHatchMinus_Clicked(object sender, EventArgs e) {
+            if (data.SandPanelRocket3 > 0)
+                sandRocketTHatchTotal.Text = (--data.SandPanelRocket3).ToString();
         }
 
-        private void SandRock1MCargoPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(sandRock1MCargoTotal.Text);
-            temp++;
-            if (temp <= 8)
-                sandRock1MCargoTotal.Text = temp.ToString();
+        private void SandRocketTHatchPlus_Clicked(object sender, EventArgs e) {
+            if (data.SandPanelRocket3 < 4)
+                sandRocketTHatchTotal.Text = (++data.SandPanelRocket3).ToString();
         }
 
-        private void SandRock1MHatchMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(sandRock1MHatchTotal.Text);
-            temp--;
-            if (temp >= 0)
-                sandRock1MHatchTotal.Text = temp.ToString();
+        private void SandRocketMCargoMinus_Clicked(object sender, EventArgs e) {
+            if (data.SandCargoRocket2 > 0)
+                sandRocketMCargoTotal.Text = (--data.SandCargoRocket2).ToString();
         }
 
-        private void SandRock1MHatchPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(sandRock1MHatchTotal.Text);
-            temp++;
-            if (temp <= 8)
-                sandRock1MHatchTotal.Text = temp.ToString();
+        private void SandRocketMCargoPlus_Clicked(object sender, EventArgs e) {
+            if (data.SandCargoRocket2 < 4)
+                sandRocketMCargoTotal.Text = (++data.SandCargoRocket2).ToString();
         }
 
-        private void SandRock1BCargoMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(sandRock1BCargoTotal.Text);
-            temp--;
-            if (temp >= 0)
-                sandRock1BCargoTotal.Text = temp.ToString();
+        private void SandRocketMHatchMinus_Clicked(object sender, EventArgs e) {
+            if (data.SandPanelRocket2 > 0)
+                sandRocketMHatchTotal.Text = (--data.SandPanelRocket2).ToString();
         }
 
-        private void SandRock1BCargoPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(sandRock1BCargoTotal.Text);
-            temp++;
-            if (temp <= 8)
-                sandRock1BCargoTotal.Text = temp.ToString();
+        private void SandRocketMHatchPlus_Clicked(object sender, EventArgs e) {
+            if (data.SandPanelRocket2 < 4)
+                sandRocketMHatchTotal.Text = (++data.SandPanelRocket2).ToString();
         }
 
-        private void SandRock1BHatchMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(sandRock1BHatchTotal.Text);
-            temp--;
-            if (temp >= 0)
-                sandRock1BHatchTotal.Text = temp.ToString();
+        private void SandRocketBCargoMinus_Clicked(object sender, EventArgs e) {
+            if (data.SandCargoRocket1 > 0)
+                sandRocketBCargoTotal.Text = (--data.SandCargoRocket1).ToString();
         }
 
-        private void SandRock1BHatchPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(sandRock1BHatchTotal.Text);
-            temp++;
-            if (temp <= 8)
-                sandRock1BHatchTotal.Text = temp.ToString();
+        private void SandRocketBCargoPlus_Clicked(object sender, EventArgs e) {
+            if (data.SandCargoRocket1 < 4)
+                sandRocketBCargoTotal.Text = (++data.SandCargoRocket1).ToString();
+        }
+
+        private void SandRocketBHatchMinus_Clicked(object sender, EventArgs e) {
+            if (data.SandPanelRocket1 > 0)
+                sandRocketBHatchTotal.Text = (--data.SandPanelRocket1).ToString();
+        }
+
+        private void SandRocketBHatchPlus_Clicked(object sender, EventArgs e) {
+            if (data.SandPanelRocket1 < 4)
+                sandRocketBHatchTotal.Text = (++data.SandPanelRocket1).ToString();
         }
 
         private void SandFailsHatchMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(sandFailsHatchTotal.Text);
-            temp--;
-            sandFailsHatchTotal.Text = temp.ToString();
+            if (data.SandPanelDrop > 0)
+                sandFailsHatchTotal.Text = (--data.SandPanelDrop).ToString();
         }
 
         private void SandFailsHatchPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(sandFailsHatchTotal.Text);
-            temp++;
-            sandFailsHatchTotal.Text = temp.ToString();
+            if (data.SandPanelDrop < 99)
+                sandFailsHatchTotal.Text = (++data.SandPanelDrop).ToString();
         }
 
         private void SandFailsCargoMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(sandFailsCargoTotal.Text);
-            temp--;
-            sandFailsCargoTotal.Text = temp.ToString();
+            if (data.SandCargoDrop > 0)
+                sandFailsCargoTotal.Text = (--data.SandCargoDrop).ToString();
         }
 
         private void SandFailsCargoPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(sandFailsCargoTotal.Text);
-            temp++;
-            sandFailsCargoTotal.Text = temp.ToString();
+            if (data.SandCargoDrop < 99)
+                sandFailsCargoTotal.Text = (++data.SandCargoDrop).ToString();
         }
+
         //TELEOP
-
         private void TeleCargoShipPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleShipCargoTotal.Text);
-            temp++;
-            if (temp <= 8)
-                teleShipCargoTotal.Text = temp.ToString();
-
+            if (data.TeleCargoShip < 8)
+                teleShipCargoTotal.Text = (++data.TeleCargoShip).ToString();
         }
 
         private void Tele_ShipCargoMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleShipCargoTotal.Text);
-            temp--;
-            if (temp >= 0)
-                teleShipCargoTotal.Text = temp.ToString();
+            if (data.TeleCargoShip > 0)
+                teleShipCargoTotal.Text = (--data.TeleCargoShip).ToString();
         }
 
         private void TeleShipHatchMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleShipHatchTotal.Text);
-            temp--;
-            if (temp >= 0)
-                teleShipHatchTotal.Text = temp.ToString();
+            if (data.TelePanelShip > 0)
+                teleShipHatchTotal.Text = (--data.TelePanelShip).ToString();
         }
 
         private void TeleShipHatchPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleShipHatchTotal.Text);
-            temp++;
-            if (temp <= 8)
-                teleShipHatchTotal.Text = temp.ToString();
+            if (data.TelePanelShip < 8)
+                teleShipHatchTotal.Text = (++data.TelePanelShip).ToString();
         }
 
-        private void TeleRock1TCargoMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleRock1TCargoTotal.Text);
-            temp--;
-            if (temp >= 0)
-                teleRock1TCargoTotal.Text = temp.ToString();
+        private void TeleRocketTCargoMinus_Clicked(object sender, EventArgs e) {
+            if (data.TeleCargoRocket3 > 0)
+                teleRocketTCargoTotal.Text = (--data.TeleCargoRocket3).ToString();
         }
 
-        private void TeleRock1TCargoPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleRock1TCargoTotal.Text);
-            temp++;
-            if (temp <= 8)
-                teleRock1TCargoTotal.Text = temp.ToString();
+        private void TeleRocketTCargoPlus_Clicked(object sender, EventArgs e) {
+            if (data.TeleCargoRocket3 < 4)
+                teleRocketTCargoTotal.Text = (++data.TeleCargoRocket3).ToString();
         }
 
-        private void TeleRock1THatchMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleRock1THatchTotal.Text);
-            temp--;
-            if (temp >= 0)
-                teleRock1THatchTotal.Text = temp.ToString();
+        private void TeleRocketTHatchMinus_Clicked(object sender, EventArgs e) {
+            if (data.TelePanelRocket3 > 0)
+                teleRocketTHatchTotal.Text = (--data.TelePanelRocket3).ToString();
         }
 
-        private void TeleRock1THatchPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleRock1THatchTotal.Text);
-            temp++;
-            if (temp <= 8)
-                teleRock1THatchTotal.Text = temp.ToString();
+        private void TeleRocketTHatchPlus_Clicked(object sender, EventArgs e) {
+            if (data.TelePanelRocket3 < 4)
+                teleRocketTHatchTotal.Text = (++data.TelePanelRocket3).ToString();
         }
 
-        private void TeleRock1MCargoMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleRock1MCargoTotal.Text);
-            temp--;
-            if (temp >= 0)
-                teleRock1MCargoTotal.Text = temp.ToString();
+        private void TeleRocketMCargoMinus_Clicked(object sender, EventArgs e) {
+            if (data.TeleCargoRocket2 > 0)
+                teleRocketMCargoTotal.Text = (--data.TeleCargoRocket2).ToString();
         }
 
-        private void TeleRock1MCargoPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleRock1MCargoTotal.Text);
-            temp++;
-            if (temp <= 8)
-                teleRock1MCargoTotal.Text = temp.ToString();
+        private void TeleRocketMCargoPlus_Clicked(object sender, EventArgs e) {
+            if (data.TeleCargoRocket2 < 4)
+                teleRocketMCargoTotal.Text = (++data.TeleCargoRocket2).ToString();
         }
 
-        private void TeleRock1MHatchMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleRock1MHatchTotal.Text);
-            temp--;
-            if (temp >= 0)
-                teleRock1MHatchTotal.Text = temp.ToString();
+        private void TeleRocketMHatchMinus_Clicked(object sender, EventArgs e) {
+            if (data.TelePanelRocket2 > 0)
+                teleRocketMHatchTotal.Text = (--data.TelePanelRocket2).ToString();
         }
 
-        private void TeleRock1MHatchPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleRock1MHatchTotal.Text);
-            temp++;
-            if (temp <= 8)
-                teleRock1MHatchTotal.Text = temp.ToString();
+        private void TeleRocketMHatchPlus_Clicked(object sender, EventArgs e) {
+            if (data.TelePanelRocket2 < 4)
+                teleRocketMHatchTotal.Text = (++data.TelePanelRocket2).ToString();
         }
 
-        private void TeleRock1BCargoMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleRock1BCargoTotal.Text);
-            temp--;
-            if (temp >= 0)
-                teleRock1BCargoTotal.Text = temp.ToString();
+        private void TeleRocketBCargoMinus_Clicked(object sender, EventArgs e) {
+            if (data.TeleCargoRocket1 > 0)
+                teleRocketBCargoTotal.Text = (--data.TeleCargoRocket1).ToString();
         }
 
-        private void TeleRock1BCargoPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleRock1BCargoTotal.Text);
-            temp++;
-            if (temp <= 8)
-                teleRock1BCargoTotal.Text = temp.ToString();
+        private void TeleRocketBCargoPlus_Clicked(object sender, EventArgs e) {
+            if (data.TeleCargoRocket1 < 4)
+                teleRocketBCargoTotal.Text = (++data.TeleCargoRocket1).ToString();
         }
 
-        private void TeleRock1BHatchMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleRock1BHatchTotal.Text);
-            temp--;
-            if (temp >= 0)
-                teleRock1BHatchTotal.Text = temp.ToString();
+        private void TeleRocketBHatchMinus_Clicked(object sender, EventArgs e) {
+            if (data.TelePanelRocket1 > 0)
+                teleRocketBHatchTotal.Text = (--data.TelePanelRocket1).ToString();
         }
 
-        private void TeleRock1BHatchPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleRock1BHatchTotal.Text);
-            temp++;
-            if (temp <= 8)
-                teleRock1BHatchTotal.Text = temp.ToString();
+        private void TeleRocketBHatchPlus_Clicked(object sender, EventArgs e) {
+            if (data.TelePanelRocket1 < 4)
+                teleRocketBHatchTotal.Text = (++data.TelePanelRocket1).ToString();
         }
 
         private void TeleFailsHatchMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleFailsHatchTotal.Text);
-            temp--;
-            teleFailsHatchTotal.Text = temp.ToString();
+            if (data.TelePanelDrop > 0)
+                teleFailsHatchTotal.Text = (--data.TelePanelDrop).ToString();
         }
 
         private void TeleFailsHatchPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleFailsHatchTotal.Text);
-            temp++;
-            teleFailsHatchTotal.Text = temp.ToString();
+            if (data.TelePanelDrop < 99)
+                teleFailsHatchTotal.Text = (++data.TelePanelDrop).ToString();
         }
 
         private void TeleFailsCargoMinus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleFailsCargoTotal.Text);
-            temp--;
-            teleFailsCargoTotal.Text = temp.ToString();
+            if (data.TeleCargoDrop > 0)
+                teleFailsCargoTotal.Text = (--data.TeleCargoDrop).ToString();
         }
 
         private void TeleFailsCargoPlus_Clicked(object sender, EventArgs e) {
-            int temp = int.Parse(teleFailsCargoTotal.Text);
-            temp++;
-            teleFailsCargoTotal.Text = temp.ToString();
+            if (data.TeleCargoDrop < 99)
+                teleFailsCargoTotal.Text = (++data.TeleCargoDrop).ToString();
         }
     }
 }
